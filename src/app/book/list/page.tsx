1848ba4c20c86b7c15e85d86bd537594de4d3303
刘华瑞 <1066521901@qq.com>
2026-01-30 14:03:01 +0800

    feat: 编辑&删除 前端页面

--- src/app/book/list/page.tsx
+++ src/app/book/list/page.tsx
@@ -1,16 +1,52 @@
 'use client';
 import bookApi from '@/api/book';
 import React, { useState, useEffect } from 'react';
-import { Button, Flex, Table, Image } from 'antd';
-import type { TableColumnsType, TableProps } from 'antd';
+import { Button, Flex, Table, Image, Dropdown, Space } from 'antd';
+import type { TableColumnsType, TableProps, MenuProps } from 'antd';
+import { EllipsisOutlined } from '@ant-design/icons';
 import { BookItem } from '@/types/book';
 
 type TableRowSelection<T extends object = object> = TableProps<T>['rowSelection'];
 
+const items: MenuProps['items'] = [
+  {
+    label: '编辑',
+    key: 'edit'
+  },
+  {
+    label: '删除',
+    key: 'delete',
+    danger: true
+  }
+];
+
+const menuProps = {
+  items,
+  onClick: () => {}
+};
+
 const columns: TableColumnsType<BookItem> = [
   { title: 'name', dataIndex: 'name' },
   { title: 'author', dataIndex: 'author' },
-  { title: 'description', dataIndex: 'description' },
+  {
+    title: 'description',
+    dataIndex: 'description',
+    render: (text: string) => (
+      <span
+        style={{
+          display: 'inline-block',
+          maxWidth: 150,
+          whiteSpace: 'nowrap',
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+          verticalAlign: 'middle'
+        }}
+        title={text}
+      >
+        {text}
+      </span>
+    )
+  },
   { title: 'createdAt', dataIndex: 'createdAt' },
   { title: 'publishAt', dataIndex: 'publishAt' },
   {
@@ -18,13 +54,27 @@ const columns: TableColumnsType<BookItem> = [
     dataIndex: 'cover',
     render: (text) => <Image width={200} alt="basic" src={text} />
   },
-  { title: 'stock', dataIndex: 'stock' }
+  { title: 'stock', dataIndex: 'stock' },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (_, record) => (
+      <Space wrap>
+        <Space.Compact>
+          <Button type="primary">操作</Button>
+          <Dropdown menu={menuProps} placement="bottomRight">
+            <Button type="primary" icon={<EllipsisOutlined />} />
+          </Dropdown>
+        </Space.Compact>
+      </Space>
+    )
+  }
 ];
 
 export default function Home() {
   const [bookList, setBookList] = useState<BookItem[]>([]);
   const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
-  const [loading, setLoading] = useState(false);
+  const [tableLoading, setTableLoading] = useState(false);
   const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
     setSelectedRowKeys(newSelectedRowKeys);
   };
@@ -34,10 +84,20 @@ export default function Home() {
     onChange: onSelectChange
   };
 
+  const getBookList = () => {
+    queueMicrotask(() => setTableLoading(true));
+    bookApi
+      .getBookList()
+      .then((res) => {
+        setBookList(res.data);
+      })
+      .finally(() => {
+        setTableLoading(false);
+      });
+  };
+
   useEffect(() => {
-    bookApi.getBookList().then((res) => {
-      setBookList(res.data);
-    });
+    getBookList();
   }, []);
 
   return (
@@ -45,7 +105,13 @@ export default function Home() {
       <Flex align="center" gap="middle">
         <Button type="primary">创建书籍</Button>
       </Flex>
-      <Table<BookItem> rowSelection={rowSelection} columns={columns} dataSource={bookList} />
+      <Table<BookItem>
+        loading={tableLoading}
+        scroll={{ y: 'calc(100vh - 250px)' }}
+        rowSelection={rowSelection}
+        columns={columns}
+        dataSource={bookList}
+      />
     </Flex>
   );
 }
